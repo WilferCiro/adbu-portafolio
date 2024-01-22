48507c2513cb3eb491e9f9b5d05d0c692beb2449
Wilfer Daniel Ciro Maya <ciro@DESKTOP-RLBAN4Q>
2024-01-22 18:48:05 -0500

    feat: add work list

---
+++ src/data/work.ts
@@ -0,0 +1,152 @@
+export const workList = [
+    {
+        company: {
+            name: "Centro Neuropsicológico Neurosercrecer",
+            url: null
+        },
+        position: {
+            es: "Asistente de marketing",
+            en: "Asistente de marketing"
+        },
+        dates: [new Date("2023, Juny"), "present"],
+        descrioption: {
+            es: [
+                "Monitorear redes sociales (Whatsapp, Facebook, Instagram, Tiktok, Youtube, Gmail, Hotmart)",
+                "Setter de ventas y cierres de productos digitales Low Ticket con base en el discurso de ventas establecido",
+                "Crear copys para posts y mensajes en redes sociales",
+                "Editar videos",
+                "Subir y crear historias para Instagram y Facebook con metodología AIDA enfocada en el cierre de ventas y creación de comunidad",
+                "Analizar las conversaciones en redes sociales con el fin de identificar necesidades, inconformidades o características del público objetivo",
+                "Realizar seguimiento a clientes potenciales",
+                "Recuperación de cartera con aquellos clientes que adquirieron algún producto a cuotas",
+                "Tramitar pedidos, solicitudes y reclamos, así como su pronta resolución"
+            ],
+            en: [
+                "Monitorear redes sociales (Whatsapp, Facebook, Instagram, Tiktok, Youtube, Gmail, Hotmart)",
+                "Setter de ventas y cierres de productos digitales Low Ticket con base en el discurso de ventas establecido",
+                "Crear copys para posts y mensajes en redes sociales",
+                "Editar videos",
+                "Subir y crear historias para Instagram y Facebook con metodología AIDA enfocada en el cierre de ventas y creación de comunidad",
+                "Analizar las conversaciones en redes sociales con el fin de identificar necesidades, inconformidades o características del público objetivo",
+                "Realizar seguimiento a clientes potenciales",
+                "Recuperación de cartera con aquellos clientes que adquirieron algún producto a cuotas",
+                "Tramitar pedidos, solicitudes y reclamos, así como su pronta resolución"
+            ],
+        }
+    }, 
+    {
+        company: {
+            name: "Colegio Saint Andrews",
+            url: "https://www.saintandrews.edu.co/"
+        },
+        position: {
+            es: "Docente Asistente Bilingüe",
+            en: "Docente Asistente Bilingüe"
+        },
+        dates: [new Date("2023, July"), new Date("2023, July")],
+        descrioption: {
+            es: [
+                "Apoyar a los docentes durante las actividades programadas",
+                "Hacer los reemplazos que se requieran en caso de que los docentes no puedan asistir al aula de clases",
+                "Acompañar y entusiasmar a los estudiantes en todos los eventos o actividades de la institución",
+                "Realizar refuerzos a los estudiantes que lo requieran",
+                "Hablar permanentemente en inglés procurando utilizar este idioma en todo momento con los estudiantes",
+            ],
+            en: [
+                "Apoyar a los docentes durante las actividades programadas",
+                "Hacer los reemplazos que se requieran en caso de que los docentes no puedan asistir al aula de clases",
+                "Acompañar y entusiasmar a los estudiantes en todos los eventos o actividades de la institución",
+                "Realizar refuerzos a los estudiantes que lo requieran",
+                "Hablar permanentemente en inglés procurando utilizar este idioma en todo momento con los estudiantes",
+            ],
+        }
+    }, 
+    {
+        company: {
+            name: "Ocean Pearl and Diamonds",
+            url: "https://www.oceanpearlny.com/"
+        },
+        position: {
+            es: "Diseñadora gráfica Jr.",
+            en: "Diseñadora gráfica Jr."
+        },
+        dates: [new Date("2022, March"), new Date("2022, April")],
+        descrioption: {
+            es: [
+                "Montaje de página web en plataforma Shopify",
+                "Diseño de banners publicitarios para página web",
+                "Elaboración y seguimiento del plan de trabajo de acuerdo a objetivos",
+                "Edición de imágenes",
+            ],
+            en: [
+                "Montaje de página web en plataforma Shopify",
+                "Diseño de banners publicitarios para página web",
+                "Elaboración y seguimiento del plan de trabajo de acuerdo a objetivos",
+                "Edición de imágenes",
+            ],
+        }
+    }, 
+    {
+        company: {
+            name: "Alcaldía Municipal De Alcalá",
+            url: "https://www.alcala-valle.gov.co/"
+        },
+        position: {
+            es: "Funcionaria Pública Competitividad",
+            en: "Public Official Competitiveness"
+        },
+        dates: [new Date("2021, Febrary"), new Date("2021, December")],
+        descrioption: {
+            es: [
+                "Gestión de proyectos",
+                "Ejecución de eventos comunitarios",
+                "Redacción de propuestas relacionadas con el mejoramiento de la competitividad del municipio y del sector turístico",
+                "Manejo de inventarios y bases de datos",
+                "Visitas técnicas a los prestadores de servicios turísticos",
+                "Labores ofimáticas y de contratación básica"
+            ],
+            en: [
+                "Gestión de proyectos",
+                "Ejecución de eventos comunitarios",
+                "Redacción de propuestas relacionadas con el mejoramiento de la competitividad del municipio y del sector turístico",
+                "Manejo de inventarios y bases de datos",
+                "Visitas técnicas a los prestadores de servicios turísticos",
+                "Labores ofimáticas y de contratación básica"
+            ],
+        }
+    }, 
+    {
+        company: {
+            name: "Institución Educativa Alfonso Jaramillo Gutiérrez",
+            url: "https://iealfonsojaramillo.edu.co/"
+        },
+        position: {
+            es: "Docente Practicante",
+            en: "Docente Practicante"
+        },
+        dates: [new Date("2020, September"), new Date("2020, November")],
+        descrioption: {
+            es: [
+            ],
+            en: [
+            ],
+        }
+    }, 
+    {
+        company: {
+            name: "Institución Educativa El Pital",
+            url: "https://www.iepital.aprendamoswebnova.com/public/principal/ui/uiMain.php"
+        },
+        position: {
+            es: "Docente Practicante",
+            en: "Docente Practicante"
+        },
+        dates: [new Date("2020, May"), new Date("2020, July")],
+        descrioption: {
+            es: [
+            ],
+            en: [
+            ],
+        }
+    }
+]
